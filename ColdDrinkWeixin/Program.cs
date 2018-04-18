7090f3672e7c3d86568ac5750aa9693c42826396
zhenghua75 <zhenghua9409@gmail.com>
2018-04-18 23:29:55 +0800

    发布

--- ColdDrinkWeixin/Program.cs
+++ ColdDrinkWeixin/Program.cs
@@ -4,7 +4,9 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
@@ -20,6 +22,26 @@ namespace ColdDrinkWeixin
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+                //.UseKestrel()
                 .Build();
+
+        //public static IWebHost BuildWebHost(string[] args)
+        //{
+        //    var config = new ConfigurationBuilder()
+        //        .SetBasePath(Directory.GetCurrentDirectory())
+        //        .AddJsonFile("hosting.json", optional: true)
+        //        .AddCommandLine(args)
+        //        .Build();
+
+        //    return WebHost.CreateDefaultBuilder(args)
+        //        .UseUrls("http://*:5000")
+        //        .UseConfiguration(config)
+        //        .Configure(app =>
+        //        {
+        //            app.Run(context =>
+        //                context.Response.WriteAsync("Hello, World!"));
+        //        })
+        //        .Build();
+        //}
     }
 }
