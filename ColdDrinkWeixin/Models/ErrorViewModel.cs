1120c06bac7c2f18b92cbf109c26e130665c250b
zhenghua75 <zhenghua9409@gmail.com>
2018-04-18 13:37:12 +0800

    initial commit

---
+++ ColdDrinkWeixin/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ColdDrinkWeixin.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
